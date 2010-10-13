695dc12ccdff1e51c8fd5355a98b320030b1ba1a
Jonathon McKitrick <jmckitrick@mac.com>
2010-10-13 16:46:22 -0400

    Add some git notes. Export some uffi functions.

--- defpackage.lisp
+++ defpackage.lisp
@@ -1,5 +1,9 @@
 (defpackage #:cl-hackery
+  (:nicknames #:hack)
+  (:documentation "CL hacking")
   (:use #:cl #:cffi)
   (:export
    #:run-cffi
-   #:run-list))
+   #:run-list
+   #:run-lols
+   ))
